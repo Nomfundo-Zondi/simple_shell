67b642656e0574fca95c234c848e57d2fa962982
Nomfundo <Nomfundozondi18@gmail.com>
2023-07-19 09:45:34 -0700

    Task 0 - write code that passes the Betty check

---
+++ 0-proud_betty.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("A beautiful code that passes the Betty checks.\n");
+	return (0);
+}
+
